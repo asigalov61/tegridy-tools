aa313101bb1c66f404076eab2a23e3a8999ec617
Alex <asigalov61@hotmail.com>
2021-01-26 13:02:00 -0800

    Add files via upload

--- tegridy-tools/__init__.py
+++ tegridy-tools/__init__.py
@@ -1,1 +1,15 @@
+"""
+tegridy-tools
 
+All tegridy-tools modules.
+"""
+
+from .__version__ import __version__
+
+__author__ = 'Aleksandr Sigalov'
+
+__credits__ = 'Project Los Angeles / Tegridy Code 2021'
+
+
+from . import minGPT
+from . import TMIDI
---
+++ tegridy-tools/__version__.py
@@ -0,0 +1,2 @@
+VERSION_TUPLE = (1, 0, 1)
+__version__ = ".".join(map(str, VERSION_TUPLE))
