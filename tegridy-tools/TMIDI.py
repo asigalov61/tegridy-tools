77301b5a10f09fff05224d3cf429b7ae75ee8568
Alex <asigalov61@hotmail.com>
2021-03-22 04:10:33 -0700

    Version 2.0. Updated copyright notices

--- tegridy-tools/TMIDI.py
+++ tegridy-tools/TMIDI.py
@@ -14,24 +14,26 @@
 #	pjb.com.au
 #
 #	Project Los Angeles
+#
 #	Tegridy Code 2021
+#
 #       https://github.com/Tegridy-Code/Project-Los-Angeles
 #
 #
 ###################################################################################
-   Copyright 2021 Project Los Angeles / Tegridy Code
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+#       Copyright 2021 Project Los Angeles / Tegridy Code
+#
+#       Licensed under the Apache License, Version 2.0 (the "License");
+#       you may not use this file except in compliance with the License.
+#       You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing, software
+#       distributed under the License is distributed on an "AS IS" BASIS,
+#       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#       See the License for the specific language governing permissions and
+#       limitations under the License.
 ###################################################################################
 #
 #	MIDI.py Module v.6.7. by Peter Billam
