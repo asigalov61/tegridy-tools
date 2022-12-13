a819c002902abe1c8d130ed5345a45b43f9b8643
Alex <asigalov61@hotmail.com>
2022-12-13 06:28:44 -0800

    Version 1.0 Code improvements
    Added accuracy calculation to forward function
    Added verbosity to the generator function
    Removed redundant code

--- tegridy-tools/LWA-Transformer/lwa_transformer.py
+++ tegridy-tools/LWA-Transformer/lwa_transformer.py
@@ -22,7 +22,7 @@
 import math
 
 import torch
-from torch import nn, einsum
+from torch import arccosh, nn, einsum
 import torch.nn.functional as F
 
 from einops import rearrange, repeat, pack, unpack
@@ -381,15 +381,37 @@ class LocalTransformer(nn.Module):
         n, device = prime.shape[1], prime.device
 
         out = prime
+        gt_count = 0
 
-        for _ in range(seq_len):
+        for s in range(seq_len):
+
             logits = self.forward(out[:, -self.max_seq_len:], **kwargs)
             filtered_logits = top_k(logits[:, -1], thres = filter_thres)
             probs = F.softmax(filtered_logits / temperature, dim = -1)
             sampled = torch.multinomial(probs, 1)
             out = torch.cat((out, sampled), dim = -1)
 
+            if gt_count % 64:
+              print(gt_count, '/', len(seq_len))
+
+            gt_count += 1
+
         return out[:, n:]
+
+    def compute_accuracy(self, logits, labels): 
+        out = torch.argmax(logits, dim=-1) 
+        out = out.flatten() 
+        labels = labels.flatten() 
+
+        mask = (labels != self.token_pad) 
+        out = out[mask] 
+        labels = labels[mask] 
+
+        num_right = (out == labels)
+        num_right = torch.sum(num_right).type(torch.float32)
+
+        acc = num_right / len(labels) 
+        return acc
 
     def forward(self, x, mask = None, return_loss = False):
         if return_loss:
@@ -411,86 +433,10 @@ class LocalTransformer(nn.Module):
             return logits
 
         logits = rearrange(logits, 'b n c -> b c n')
+        
         loss = F.cross_entropy(logits, labels, ignore_index = self.ignore_index)
-        return loss
+        acc = self.compute_accuracy(logits, labels)
+
+        return loss, acc
 
 #===================================================================================================================
-
-# helper function
-
-def exists(val):
-    return val is not None
-
-
-def eval_decorator(fn):
-    def inner(model, *args, **kwargs):
-        was_training = model.training
-        model.eval()
-        out = fn(model, *args, **kwargs)
-        model.train(was_training)
-        return out
-
-    return inner
-
-
-# top k filtering
-
-
-def top_k(logits, thres=0.9):
-    k = int((1 - thres) * logits.shape[-1])
-    val, ind = torch.topk(logits, k)
-    probs = torch.full_like(logits, float("-inf"))
-    probs.scatter_(1, ind, val)
-    return probs
-
-
-class AutoregressiveWrapper(nn.Module):
-    def __init__(self, net, pad_value=0):
-        super().__init__()
-        self.max_seq_len = net.max_seq_len
-        self.pad_value = pad_value
-        self.net = net
-
-    @torch.no_grad()
-    @eval_decorator
-    def generate(
-        self,
-        start_tokens,
-        seq_len,
-        eos_token=None,
-        temperature=1.0,
-        filter_thres=0.9,
-        **kwargs
-    ):
-        b, n, device = *start_tokens.shape, start_tokens.device
-
-        out = start_tokens
-
-        for _ in range(seq_len):
-            logits = self.net(
-                out[:, -self.max_seq_len:],
-                **kwargs
-            )[:, -1]
-
-            filtered_logits = top_k(logits, thres = filter_thres)
-            probs = F.softmax(filtered_logits / temperature, dim=-1)
-
-            sample = torch.multinomial(probs, 1)
-            out = torch.cat((out, sample), dim=-1)
-
-            if exists(eos_token):
-                is_eos_token = out == eos_token
-
-                if is_eos_token.any(dim=-1).all():
-                    # mask out everything after the eos tokens
-                    shifted_is_eos_tokens = F.pad(is_eos_tokens, (1, -1))
-                    mask = shifted_is_eos_tokens.float().cumsum(dim=-1) >= 1
-                    out = out.masked_fill(mask, self.pad_value)
-                    break
-
-        out = out[:, n:]
-        return out
-
-    def forward(self, x, **kwargs):
-        x_inp = x[:, :-1]
-        return self.net(x_inp, **kwargs)
